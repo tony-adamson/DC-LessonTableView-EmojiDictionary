18a991d8a5394c09ef4811eb5c90b451f5cc54c3
Anton <antony.adamson@gmail.com>
2023-04-22 00:57:42 +0600

    Первоначальная настройка приложения

---
+++ DC-LessonTableView-EmojiDictionary/Model/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  DC-LessonTableView-EmojiDictionary
+//
+//  Created by Антон Адамсон on 22.04.2023.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
