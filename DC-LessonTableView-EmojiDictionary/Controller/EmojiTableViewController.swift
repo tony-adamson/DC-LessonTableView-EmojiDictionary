0b2aee388230325a58febd004d004584d67686be
Anton <antony.adamson@gmail.com>
2023-04-22 01:39:32 +0600

    Implement the Data Source

--- DC-LessonTableView-EmojiDictionary/Controller/EmojiTableViewController.swift
+++ DC-LessonTableView-EmojiDictionary/Controller/EmojiTableViewController.swift
@@ -9,6 +9,38 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
+    var emojis: [Emoji] = [
+       Emoji(symbol: "😀", name: "Grinning Face",
+             description: "A typical smiley face.", usage: "happiness"),
+       Emoji(symbol: "😕", name: "Confused Face",
+             description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
+       Emoji(symbol: "😍", name: "Heart Eyes",
+             description: "A smiley face with hearts for eyes.", usage: "love of something; attractive"),
+       Emoji(symbol: "🧑‍💻", name: "Developer",
+             description: "A person working on a MacBook (probably using Xcode to write iOS apps in Swift).",
+             usage: "apps, software, programming"),
+       Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "something slow"),
+       Emoji(symbol: "🐘", name: "Elephant", description: "A gray elephant.", usage: "good memory"),
+       Emoji(symbol: "🍝", name: "Spaghetti", description: "A plate of spaghetti.", usage: "spaghetti"),
+       Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
+       Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
+       Emoji(symbol: "📚", name: "Stack of Books", description: "Three colored books stacked on each other.",
+             usage: "homework, studying"),
+       Emoji(symbol: "💔", name: "Broken Heart", description: "A red, broken heart.",
+             usage: "extreme sadness"),
+       Emoji(symbol: "💤", name: "Snore", description: "Three blue \'z\'s.", usage: "tired, sleepiness"),
+       Emoji(symbol: "🏁", name: "Checkered Flag",  description: "A black-and-white checkered flag.",
+             usage: "completion"),
+       Emoji(symbol: "💩", name: "Shit", description: "Big mountain of shit", usage: "Very bad situation"),
+       Emoji(symbol: "🤡", name: "Clown", description: "Man - who think development its easy",
+             usage: "only for holywars"),
+       Emoji(symbol: "🧠", name: "Brain", description: "IQ 150", usage: "rarely"),
+       Emoji(symbol: "🦾", name: "ChatGPT", description: "Thing thing is smarter then you",
+             usage: "never"),
+       Emoji(symbol: "🐸", name: "pepe", description: "green frog from memes", usage: "regullary"),
+       Emoji(symbol: "🍔", name: "BigMac", description: "Many calories, junk food", usage: "every day")
+    ]
+             
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,24 +54,27 @@ class EmojiTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // Количество секций - 1
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // возвращаем столько строк, сколько элементво в массиве emojis
+        return emojis.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
         // Configure the cell...
-
+        let emoji = emojis[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(emoji.symbol) - \(emoji.name)"
+        content.secondaryText = "\(emoji.description)"
+        cell.contentConfiguration = content
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
